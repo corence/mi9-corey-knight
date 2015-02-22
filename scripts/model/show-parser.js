f5bf48ec759623a5df96e802e845e2657fc06219
Corey Knight <corence@gmail.com>
2015-02-22 22:15:16 +1100

    now passing for 3 out of 5 scenarios -- just need exception handling

--- scripts/model/show-parser.js
+++ scripts/model/show-parser.js
@@ -4,12 +4,16 @@
 module.exports = {
 	extract_available_shows: function(doc) {
 				var response = doc.payload.map(function(show) {
+					if(!show.drm) return null;
+					if(show.episodeCount <= 0) return null;
+					
 					return {
 						"image": show.image.showImage,
 						"slug": show.slug,
 						"title": show.title,
 					};
 				});
+				response = response.filter(function(x) { return x !== null; });
 				return {"response": response};
 			}
 };
